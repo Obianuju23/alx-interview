0d2b54511878bb45ef812adec95d5a4930a5d480
Obianuju23 <obianujunmoh@gmail.com>
2023-10-15 20:49:32 +0100

    minimum operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -15,13 +15,19 @@ def minOperations(n):
     returns the minimum operations to get n H's
     '''
     min_operations = 0
+    paste_buffer = 0
 
     if n <= 1:
         return min_operations
 
     for i in range(2, n + 1):
         while n % i == 0:
-            min_operations += i
+            if paste_buffer < 0:
+                min_operations += i
+                paste_buffer -= i
+            else:
+                paste_buffer = i
             n /= i
 
+    min_operation += paste_buffer
     return min_operations
