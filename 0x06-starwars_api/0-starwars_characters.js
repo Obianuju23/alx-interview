1dafc805fbfab6e156716600696cda39228807ed
Obianuju23 <obianujunmoh@gmail.com>
2023-11-09 05:21:13 +0100

    script that prints all characters of a Star Wars movie

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,13 +2,13 @@
 // This script that prints all characters of a Star Wars movie
 
 const request = require('request');
-const movie_Id = process.argv[2];
+const movieId = process.argv[2];
 const filmsApiUrl = 'https://swapi-api.hbtn.io/api/films';
 
 function fetchJSON (url) {
   return new Promise(function (resolve, reject) {
     request(url, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
+      if (!error && response.statusCode === 200) {
         resolve(JSON.parse(body));
       } else {
         reject(error);
@@ -17,9 +17,9 @@ function fetchJSON (url) {
   });
 }
 
-async function printStarWarsCharacters (movie_Id) {
+async function printStarWarsCharacters (movieId) {
   try {
-    const filmData = await fetchJSON(`${filmsApiUrl}/${movie_Id}`);
+    const filmData = await fetchJSON(`${filmsApiUrl}/${movieId}`);
     for (const characterUrl of filmData.characters) {
       try {
         const characterData = await fetchJSON(characterUrl);
@@ -33,4 +33,4 @@ async function printStarWarsCharacters (movie_Id) {
   }
 }
 
-printStarWarsCharacters(movie_Id);
+printStarWarsCharacters(movieId);
